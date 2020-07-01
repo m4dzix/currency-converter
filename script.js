220a57295c3beab8d3034f182d9789d6d9d97dc0
Magdalena Checinski <madzix.p730@gmail.com>
2020-07-01 21:17:39 +0200

    Added "switch" to calculate the value of different currencies

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+let formElement = document.querySelector(".js-form");
 let amount = document.querySelector(".js-amount");
+let currencyHave = document.querySelector(".js-exchangeOn")
 let currencyWant = document.querySelector(".js-exchangeFor");
 let resultValue = document.querySelector(".js-result");
-let formElement = document.querySelector(".js-form");
-
+let zloty = 1;
 let euro = 4.4395;
 let usDollar = 3.9526;
 let britishPound = 4.9062;
@@ -11,25 +12,47 @@ let swissFranc = 4.1738;
 formElement.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  let currency = currencyWant.value;
+  let changeCurrencyFrom = currencyHave.value;
+  let changeCurrencyOn = currencyWant.value;
   let amountValue = amount.value;
   let finalResult;
 
-  switch (currency) {
+  switch (changeCurrencyFrom) {
+    case "PLN":
+      finalResult = +amountValue * zloty;
+      break;
     case "EUR":
-      finalResult = +amountValue / euro;
+      finalResult = +amountValue * euro;
       break;
     case "USD":
-      finalResult = +amountValue / usDollar;
+      finalResult = +amountValue * usDollar;
       break;
     case "GBP":
-      finalResult = +amountValue / britishPound;
+      finalResult = +amountValue * britishPound;
       break;
     case "CHF":
-      finalResult = +amountValue / swissFranc;
+      finalResult = +amountValue * swissFranc;
       break;
   }
-  resultValue.innerText = `${amountValue} PLN to ${finalResult.toFixed(
+  switch (changeCurrencyOn) {
+    case "PLN":
+      finalResult = finalResult /= zloty;
+      break;
+    case "EUR":
+      finalResult = finalResult /= euro;
+      break;
+    case "USD":
+      finalResult = finalResult /= usDollar;
+      break;
+    case "GBP":
+      finalResult = finalResult /= britishPound;
+      break;
+    case "CHF":
+      finalResult = finalResult /= swissFranc;
+      break;
+  }
+
+  resultValue.innerText = `${amountValue} ${changeCurrencyFrom} TO ${finalResult.toFixed(
     2
-  )} ${currency}`;
+  )} ${changeCurrencyOn}`;
 });
